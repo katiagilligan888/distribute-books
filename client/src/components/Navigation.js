030f27de54e090a39190d282248729e15de19afd
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-27 12:50:26 -0600

    Nav conditionally rendering if user signed in

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,22 +1,45 @@
-import React from 'react'; 
-import { Link } from 'react-router-dom';
-import SignOutButton from "./SignOut"; 
-import * as routes from '../constants/routes'; 
+import React from "react";
+import { Link } from "react-router-dom";
+import SignOutButton from "./SignOut";
+import * as routes from "../constants/routes";
 
-
-const Navigation = () => {
-    return (
-        <div className = "navigation">
-            <ul>
-                <li><Link to = {routes.SIGN_IN}>Sign In</Link></li>
-                <li><Link to = {routes.LANDING}>Landing</Link></li>
-                <li><Link to = {routes.HOME}>Home</Link></li>
-                <li><Link to = {routes.ACCOUNT}>Account</Link></li>
-                <SignOutButton />
-            </ul>
-        
-        </div>
-    )
+const Navigation = ({ authUser }) => {
+  return <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>;
 };
 
-export default Navigation; 
+const NavigationAuth = () => {
+    return(
+    <div>
+        <ul>
+            <li>
+                <Link to={routes.LANDING}>Landing</Link>
+            </li>
+            <li>
+                <Link to={routes.HOME}>Home</Link>
+            </li>
+            <li>
+                <Link to={routes.ACCOUNT}>Account</Link>
+            </li>
+            <SignOutButton />
+        </ul>
+    </div>
+    )
+  
+};
+
+const NavigationNonAuth = () => {
+return(
+    <div>
+        <ul>
+            <li>
+                <Link to={routes.SIGN_IN}>Sign In</Link>
+            </li>
+            <li>
+                <Link to={routes.LANDING}>Landing</Link>
+            </li>
+        </ul>
+    </div>
+)
+ 
+};
+export default Navigation;
--- client/src/firebase/auth.js
+++ client/src/firebase/auth.js
@@ -18,11 +18,8 @@ export const doSignOut = () => auth.signOut();
 
 
 // Password Reset
-export const doPasswordReset = (email) => {
-    auth.sendPasswordResetEmail(email);
-}
+export const doPasswordReset = (email) => auth.sendPasswordResetEmail(email);
+
 
 // Password Change
-export const doPasswordUpdate = (password) => {
-    auth.currentUser.updatePassword(password); 
-}
+export const doPasswordUpdate = (password) => auth.currentUser.updatePassword(password); 
