e37a61382cd8ac3aff0d72120611f7f5a1d62f7d
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-27 14:26:13 -0600

    Setup password reset link in Sign In

--- client/src/components/PasswordForget.js
+++ client/src/components/PasswordForget.js
@@ -1,11 +1,90 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const PasswordForget = () => {
+import { auth } from "../firebase";
+import * as routes from "../constants/routes";
+
+const PasswordForgetPage = () => {
   return (
     <div>
-      <h1>Forgot Password</h1>
+      <h1>Forgot Password </h1>
+      <PasswordForgetForm />
     </div>
   );
 };
 
-export default PasswordForget;
+const INITIAL_STATE = {
+  email: "",
+  error: null
+};
+
+class PasswordForgetForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...INITIAL_STATE
+    };
+  }
+
+  onChangeHandler = event => {
+    this.setState({
+      email: event.target.value
+    });
+  };
+
+  onSubmitHandler = event => {
+    event.preventDefault();
+    const { email } = this.state;
+
+    auth
+      .doPasswordReset(email)
+      .then(() => {
+        this.setState({ ...INITIAL_STATE });
+      })
+      .catch(err => {
+        this.setState({ error: err });
+      });
+  };
+
+  render() {
+    const { email, error } = this.state;
+
+    const isInvalid = email === "";
+
+    return (
+      <div>
+        <form onSubmit={this.onSubmitHandler}>
+          <input
+            onChange={this.onChangeHandler}
+            type="text"
+            value={email}
+            placeholder="Email Address"
+          />
+          <button disabled={isInvalid} type="submit">
+            {" "}
+            Reset my Password{" "}
+          </button>
+
+          {error && <p>{error.message}</p>}
+        </form>
+      </div>
+    );
+  }
+}
+
+const PasswordForgetLink = () => {
+  return (
+    <div>
+      <p> 
+        <Link to = {routes.PASSWORD_FORGET}> Forgot Password? </Link>
+        </p>
+      </div>
+  )
+}
+
+export default PasswordForgetPage;
+
+export {
+  PasswordForgetLink,
+  PasswordForgetForm
+}
--- client/src/components/SignInPage.js
+++ client/src/components/SignInPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
 import { SignUpLink } from './SignUpPage';
+import { PasswordForgetLink } from './PasswordForget'; 
 import { auth } from '../firebase';
 import * as routes from '../constants/routes';
 
@@ -9,9 +10,9 @@ const SignInPage = ({ history }) =>
   <div>
     <h1>Sign In</h1>
     <SignInForm history={history} />
+    <PasswordForgetLink />
     <SignUpLink />
   </div>
-
 
 
 const INITIAL_STATE = {
