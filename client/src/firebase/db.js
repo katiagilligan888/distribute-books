e4f1fc5362b8d082ce65fd7df23dd2aa50bf082f
evgeniibaibourine <eugene.baibourine@tanium.com>
2018-10-17 15:33:29 -0400

    fix up writing to the database

--- client/src/firebase/db.js
+++ client/src/firebase/db.js
@@ -1,35 +1,47 @@
 
 import { db } from './firebase';
+const BOOK_DISTRIBUTIONS = 'book-distributions';
+const BOOK_SCORES = 'book-scores';
+const USERS = 'users';
 
-
-export const doCreateUser = (userid, username, email, temple, city) =>
-  db.collection('users').doc(userid).set({
+export const doCreateUser = (userid, username, email, temple, city) => {
+  return db.collection(USERS).doc(userid).set({
     username: username,
     email: email, 
     temple: temple, 
     city: city
-  })
+  });
+}
  
 
-export const doCreateBook  = (userid, epochDate,humanDate, distributionType, numberDistributors,bookNumber, books) => 
-  db.collection('distribute-books').doc(userid).collection('date').doc(epochDate).collection('eventID').doc().set({
-    bookNumber: bookNumber, 
+export const doCreateBook  = (userid, epochDate, humanDate, distributionType, numberDistributors, bookNumber, books) =>  {
+  let userRef = db.collection(BOOK_DISTRIBUTIONS).doc(userid);
+  let finalNode = db.collection(BOOK_DISTRIBUTIONS).doc(userRef.id).collection(epochDate).doc();
+  return finalNode.set({
+      bookNumber: bookNumber, 
+      userId: userid, 
+      epochDate: epochDate, 
+      date: humanDate,
+      distributionType: distributionType, 
+      numberDistributors: numberDistributors, 
+      books: books
+  });
+}
+
+export const doCreateBookScore = (userid, epochDate, humanDate, distributionType, numberDistributors, books) => {
+  let bookCount = books.map((book) => {
+    return parseInt(book.number);
+  }).reduce((a, b) => {
+    return a + b;
+  }, 0);
+
+  return db.collection(BOOK_SCORES).add({
     userId: userid, 
     epochDate: epochDate, 
     date: humanDate,
     distributionType: distributionType, 
     numberDistributors: numberDistributors, 
-    books: books
-  })
-
-export const doCreateBookScore = (userid, epochDate, humanDate, distributionType, numberDistributors, books) => 
-  db.collection('book-scores').add({
-    userId: userid, 
-    epochDate: epochDate, 
-    date: humanDate,
-    distributionType: distributionType, 
-    numberDistributors: numberDistributors, 
-    books: books 
-  })
-
-  
+    books: books,
+    bookCount: bookCount
+  });
+}
