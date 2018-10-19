14f886b3584c49fc83f0689eeaeb944d8b0d810b
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-10-19 14:49:30 -0600

    Added redux dependencies and hooked up store and provider

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./assets/theme-dashboard/v4/dist/toolkit-inverse.css"
+import "./assets/theme-dashboard/v4/dist/toolkit-inverse.css"; 
+import { CreateStore } from 'redux'; 
+import { Provider } from 'react-redux'; 
+
+const store = createStore(() => {})
+// reducer needs to be added 
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store = {store}>
+    <Router>
+      <App />
+    </Router>,  
+  </Provider>
   document.getElementById("root")
 );
 registerServiceWorker();
