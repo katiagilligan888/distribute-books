091cc37ae6332d57d1ba7e63d5f86ba1c638eb60
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-10-20 16:16:53 -0600

    Added redux form field components in BookForm

--- client/src/components/BookForm.js
+++ client/src/components/BookForm.js
@@ -1,146 +1,159 @@
 import React from "react";
 import { db, auth } from "../firebase";
+import { Field, FieldArray, reduxForm } from "redux-form";
 import firebase from "firebase";
 import * as routes from "../constants/routes";
-import moment from 'moment'; 
-
-const INITIAL_STATE = {
-  date: "",
-  distributionType: "",
-  numberDistributors: "",
-  bookInputs: ["book-0"],
-  books: [],
-  bookTitle: "",
-  bookLanguage: "",
-  bookNumber: "", 
-  epochTime: ""
-};
+// import moment from 'moment';
 
 class BookForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...INITIAL_STATE
-    };
+  // onSubmitHandler = event => {
+  //   const {
+  //     date,
+  //     distributionType,
+  //     numberDistributors,
+  //     bookTitle,
+  //     bookLanguage,
+  //     bookNumber,
+  //     books
+  //   } = this.state;
+
+  //   event.preventDefault();
+  //   const user = firebase.auth().currentUser;
+  //   if (user) {
+  //     if (bookTitle || bookLanguage || bookNumber) {
+  //       const book = {
+  //         bookTitle: this.state.bookTitle,
+  //         bookLanguage: this.state.bookLanguage,
+  //         bookNumber: this.state.bookNumber
+  //       };
+  //       const booksArr = [...this.state.books, book];
+  //       this.setState({
+  //         books: booksArr,
+  //         bookTitle: "",
+  //         bookLanguage: "",
+  //         bookNumber: ""
+  //       });
+  //       db.doCreateBook(
+  //         user.uid,
+  //         // `${moment(`${date} 12:00`,'YYYY/MM/DD H:mm').valueOf()}`,
+  //         date,
+  //         date,
+  //         distributionType,
+  //         numberDistributors,
+  //         bookNumber,
+  //         booksArr.map(book => {
+  //           return {
+  //             title: book.bookTitle,
+  //             language: book.bookLanguage,
+  //             number: book.bookNumber
+  //           };
+  //         })
+  //       )
+  //       db.doCreateBookScore(
+  //         user.uid,
+  //         // `${moment(`${date} 12:00`,'YYYY/MM/DD H:mm').valueOf()}`,
+  //         date,
+  //         date,
+  //         distributionType,
+  //         numberDistributors,
+  //         booksArr.map(book => {
+  //           return {
+  //             title: book.bookTitle,
+  //             language: book.bookLanguage,
+  //             number: book.bookNumber
+  //           };
+  //         })
+  //       )
+  //         .then(() => {
+  //           this.setState({ ...INITIAL_STATE });
+  //           this.props.history.push(routes.HOME);
+  //         })
+  //         .catch(error => {
+  //           this.setState({ error });
+  //           console.log(this.state.error);
+  //         });
+  //     } else {
+  //       db.doCreateBook(
+  //         user.uid,
+  //         date,
+  //         distributionType,
+  //         numberDistributors,
+  //         books.map(book => {
+  //           return {
+  //             title: book.bookTitle,
+  //             language: book.bookLanguage,
+  //             number: book.bookNumber
+  //           };
+  //         })
+  //       )
+  //         .then(() => {
+  //           this.setState({ ...INITIAL_STATE });
+  //         })
+  //         .catch(error => {
+  //           this.setState({ error });
+  //           console.log(this.state.error);
+  //         });
+  //     }
+  //   }
+  // };
+
+  renderField = field => {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type={field.inputType}
+          {...field.input}
+        />
+        {field.meta.touched ? field.meta.error : ""}
+      </div>
+    );
+  };
+
+  renderBookSubFields = (book, index, fields) => {
+    return (
+      <li key={index}>
+        <button type="button">Remove Book</button>
+        <h4>Book #{index + 1}</h4>
+        <Field
+          name={`${book}.title`}
+          component={this.renderField}
+          label="Book Title"
+          inputType="text"
+        />
+        <Field
+          name={`${book}.language`}
+          component={this.renderField}
+          label="Book Language"
+          inputType="text"
+        />
+        <Field
+          name={`${book}.number`}
+          component={this.renderField}
+          label="Book Number"
+          inputType="number"
+        />
+      </li>
+    );
+  };
+
+  renderBooks = ({ fields }) => {
+    return (
+      <ul>
+        {fields.map(this.renderBookSubFields)}
+        <button type = "button" onClick = {() => fields.push({})}>Add Book</button>
+      </ul>
+    )
   }
 
-  onChangeHandler = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  };
-
-  appendBookInput = e => {
-    e.preventDefault();
-    const book = {
-      bookTitle: this.state.bookTitle,
-      bookLanguage: this.state.bookLanguage,
-      bookNumber: this.state.bookNumber
-    };
-
-    const booksArr = [...this.state.books, book];
-
-    this.setState({
-      bookInputs: this.state.bookInputs.concat([
-        `book-${this.state.bookInputs.length}`
-      ]),
-      books: booksArr,
-      bookTitle: "",
-      bookLanguage: "",
-      bookNumber: ""
-    });
-  };
-
-  onSubmitHandler = event => {
-    const {
-      date,
-      distributionType,
-      numberDistributors,
-      bookTitle,
-      bookLanguage,
-      bookNumber,
-      books
-    } = this.state;
-
-    event.preventDefault();
-    const user = firebase.auth().currentUser;
-    if (user) {
-      if (bookTitle || bookLanguage || bookNumber) {
-        const book = {
-          bookTitle: this.state.bookTitle,
-          bookLanguage: this.state.bookLanguage,
-          bookNumber: this.state.bookNumber
-        };
-        const booksArr = [...this.state.books, book];
-        this.setState({
-          books: booksArr,
-          bookTitle: "",
-          bookLanguage: "",
-          bookNumber: ""
-        });
-        db.doCreateBook(
-          user.uid,
-          `${moment(`${date} 12:00`,'YYYY/MM/DD H:mm').valueOf()}`, 
-          date,
-          distributionType,
-          numberDistributors,
-          bookNumber, 
-          booksArr.map(book => {
-            return {
-              title: book.bookTitle,
-              language: book.bookLanguage,
-              number: book.bookNumber
-            };
-          })
-        )
-        db.doCreateBookScore(
-          user.uid,
-          `${moment(`${date} 12:00`,'YYYY/MM/DD H:mm').valueOf()}`, 
-          date,
-          distributionType,
-          numberDistributors,
-          booksArr.map(book => {
-            return {
-              title: book.bookTitle,
-              language: book.bookLanguage,
-              number: book.bookNumber
-            };
-          })
-        )
-          .then(() => {
-            this.setState({ ...INITIAL_STATE });
-            this.props.history.push(routes.HOME); 
-          })
-          .catch(error => {
-            this.setState({ error });
-            console.log(this.state.error);
-          });
-      } else {
-        db.doCreateBook(
-          user.uid,
-          date,
-          distributionType,
-          numberDistributors,
-          books.map(book => {
-            return {
-              title: book.bookTitle,
-              language: book.bookLanguage,
-              number: book.bookNumber
-            };
-          })
-        )
-          .then(() => {
-            this.setState({ ...INITIAL_STATE });
-          })
-          .catch(error => {
-            this.setState({ error });
-            console.log(this.state.error);
-          });
-      }
-    }
+  onSubmit = values => {
+    console.log(values);
   };
 
   render() {
+    const { handleSubmit } = this.props;
+
     return (
       <div className="bookform">
         <h1>Book Form </h1>
@@ -149,105 +162,52 @@ class BookForm extends React.Component {
           <strong>Instructions:</strong> Fill out the form with the information
           about the latest booksale{" "}
         </h2>
-        <form onSubmit={this.onSubmitHandler}>
-          <div className="form-row justify-content-center">
-            <div className="form-group col-md-2">
-              <label for="dateOfEvent"> Date of Event</label>
-              <input
-                className="form-control"
-                value={this.state.date}
-                name="date"
-                type="date"
-                onChange={this.onChangeHandler}
-                label="Date"
-              />
-            </div>
-            <div className="form-group col-md-3">
-              <label for="numberOfDistributors">Number of Distibutors</label>
-              <input
-                className="form-control"
-                value={this.state.numberDistributors}
-                name="numberDistributors"
-                type="number"
-                onChange={this.onChangeHandler}
-                label="Number of Distributors"
-              />
-            </div>
-            <div className="form-group col-md-5">
-              <label for="distributionType">Type of Book Distribution </label>
-              <select
-                className="form-control"
-                value={this.state.distributionType}
-                name="distributionType"
-                onChange={this.onChangeHandler}
-              >
-                <option value="" disabled selected>
-                  Choose Your Distribution Type{" "}
-                </option>
-                <option value="option 1"> Option 1 </option>
-                <option value="option 2"> Option 2</option>
-                <option value="option 3"> Option 3 </option>
-                <option value="option 4"> Option 4 </option>
-              </select>
-            </div>
-            {this.state.bookInputs.map((input, index) => (
-              <div className="dynamicBooks">
-                <div className="form-row">
-                  <div className="form-group col-md-4">
-                    <label for="bookTitle">Book Title</label>
-                    <input
-                      className="form-control"
-                      name="bookTitle"
-                      type="text"
-                      onChange={this.onChangeHandler}
-                      label="Book Title"
-                    />
-                  </div>
-                  <div className="form-group col-md-4">
-                    <label for="bookLanguage">Book Language</label>
-                    <input
-                      className="form-control"
-                      name="bookLanguage"
-                      type="text"
-                      onChange={this.onChangeHandler}
-                      label="Book Language"
-                    />
-                  </div>
-                  <div className="form-group col-md-4">
-                    <label for="bookNumber">Number of Books</label>
-                    <input
-                      className="form-control"
-                      name="bookNumber"
-                      type="number"
-                      onChange={this.onChangeHandler}
-                      label="Number of Books"
-                    />
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-          <button
-            type="button"
-            className="btn btn-pill btn-success button-add-book"
-            onClick={this.appendBookInput}
-          >
-            {" "}
-            Add Another Book{" "}
+        <form onSubmit={handleSubmit(this.onSubmit)}>
+          <Field
+            name="date"
+            component={this.renderField}
+            label="Date of Event"
+            inputType="date"
+          />
+          <Field
+            name="numberDistributors"
+            component={this.renderField}
+            label="Number of Distributors"
+            inputType="number"
+          />
+          <Field name = "typeOfDistribution"
+          component = {this.renderField}
+          label = "Type of Distribution"
+          inputType = "text"
+          />
+          <FieldArray name = "books" component = {this.renderBooks}/>
+
+          <button type="submit" className="btn btn-primary">
+            Submit
           </button>
-          <div className="button-container">
-            <button
-              type="button"
-              className="btn btn-lg btn-pill btn-primary"
-              type="submit"
-            >
-              Add Book Data
-            </button>
-          </div>
         </form>
       </div>
     );
   }
 }
 
-export default BookForm;
+const validate = values => {
+  const errors = {};
+
+  if (!values.date) {
+    errors.date = "Enter a valid date";
+  }
+  if (!values.numberDistributors) {
+    errors.numberDistributors = "Enter a valid number of distributors";
+  }
+  if (!values.bookNumber) {
+    errors.bookNumber = "Enter a valid number of books";
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  validate,
+  form: "BookForm"
+})(BookForm);
