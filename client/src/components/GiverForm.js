28a738d260b0d32e21abcf3b7be3555f08176a50
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-10-31 17:11:24 -0600

    Added email verification and cleaned up Giver Form

--- client/src/components/GiverForm.js
+++ client/src/components/GiverForm.js
@@ -1,24 +1,27 @@
 import React from "react";
-import {Field, reduxForm} from 'redux-form'; 
-import Recaptcha from 'react-recaptcha';  
-import { toast } from 'react-toastify'; 
+import { Field, reduxForm } from "redux-form";
+import Recaptcha from "react-recaptcha";
+import { toast } from "react-toastify";
 import { db } from "../firebase";
-import giverLogo from '../giver_logo.png'; 
+import giverLogo from "../giver_logo.png";
 
 class GiverForm extends React.Component {
   constructor() {
     super();
     this.state = {
-      coords: '',
+      coords: ""
     };
   }
 
   componentDidMount(props) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.setState({ coords: position.coords });
-    }, (err) => {
-      console.log('Geolocation blocked: ' + err);
-    })
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ coords: position.coords });
+      },
+      err => {
+        console.log("Geolocation blocked: " + err);
+      }
+    );
   }
 
   renderField = field => {
@@ -37,25 +40,55 @@ class GiverForm extends React.Component {
         )}
       </div>
     );
-  }
+  };
 
-  onSubmit = (values) => {
-    db.doCreateGiver(values.firstName, values.lastName, values.email, values.country, this.state.coords.longitude, this.state.coords.latitude).then(() => {
-      toast.info("Thank you for becoming a Giver")
-      this.props.reset(); 
-    })
-    }
+  onSubmit = values => {
+    db.doCreateGiver(
+      values.firstName,
+      values.lastName,
+      values.email,
+      values.country,
+      this.state.coords.longitude,
+      this.state.coords.latitude
+    ).then(() => {
+      toast.info("Thank you for becoming a Giver");
+      this.props.reset();
+    });
+  };
 
   render() {
     return (
       <div className="giver-form">
-        <img className = "giver-logo" src = {giverLogo} />
-        <form className = "giver-form" onSubmit = {this.props.handleSubmit(this.onSubmit)}>
-          <Field name = "firstName" label = "First Name" inputType = "text" component = {this.renderField} />
-          <Field name = "lastName" label = "Last Name" inputType = "text" component = {this.renderField} />
-          <Field name = "email" label = "Email" inputType = "text" component = {this.renderField} />
-          <Field name = "country" label = "Country" inputType = "text" component = {this.renderField} />
-          <button className = "btn btn-primary mx-auto d-block">Sign Up</button>
+        <img className="giver-logo" src={giverLogo} />
+        <form
+          className="giver-form"
+          onSubmit={this.props.handleSubmit(this.onSubmit)}
+        >
+          <Field
+            name="firstName"
+            label="First Name"
+            inputType="text"
+            component={this.renderField}
+          />
+          <Field
+            name="lastName"
+            label="Last Name"
+            inputType="text"
+            component={this.renderField}
+          />
+          <Field
+            name="email"
+            label="Email"
+            inputType="text"
+            component={this.renderField}
+          />
+          <Field
+            name="country"
+            label="Country"
+            inputType="text"
+            component={this.renderField}
+          />
+          <button className="btn btn-primary mx-auto d-block">Sign Up</button>
         </form>
       </div>
     );
@@ -63,35 +96,39 @@ class GiverForm extends React.Component {
 }
 
 const validate = values => {
-  const errors = {}
-  if(!values.firstName){
-    errors.firstName = "Please enter name"
-  }else if(values.firstName.length < 3){
-    errors.firstName = "First name needs to be at least 3 characters"
-  }else if(values.firstName > 20){
-    errors.firstName = "First name can't exceed 20 characters"
+  const errors = {};
+  if (!values.firstName) {
+    errors.firstName = "Please enter name";
+  } else if (values.firstName.length < 3) {
+    errors.firstName = "First name needs to be at least 3 characters";
+  } else if (values.firstName > 20) {
+    errors.firstName = "First name can't exceed 20 characters";
   }
 
-  if(!values.lastName){
-    errors.lastName = "Please enter name"
-  }else if(values.lastName.length < 3){
-    errors.lastName = "Last name needs to be at least 3 characters"
-  }else if(values.lastName > 25){
-    errors.lastName = "Last name can't exceed 25 characters"
+  if (!values.lastName) {
+    errors.lastName = "Please enter name";
+  } else if (values.lastName.length < 3) {
+    errors.lastName = "Last name needs to be at least 3 characters";
+  } else if (values.lastName > 25) {
+    errors.lastName = "Last name can't exceed 25 characters";
   }
 
-  if(!values.email){
-    errors.email = "Please enter email"
+  if (!values.email) {
+    errors.email = "Please enter email";
+  } else if (values.email.includes("@") === false) {
+    errors.email = "Please enter a valid email address";
+  } else if (values.email.length < 9) {
+    errors.email = "Please enter a valid email address";
   }
 
-  if(!values.country){
-    errors.country = "Please enter country"
+  if (!values.country) {
+    errors.country = "Please enter country";
   }
 
   return errors;
-}
+};
 
 export default reduxForm({
-  validate, 
-  form: 'GiverForm'
+  validate,
+  form: "GiverForm"
 })(GiverForm);
