63b389fb36a1829f5a94565a745afe3a33ad9953
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-11-02 21:45:28 -0600

    Fixed sign in button color back to primary w/ reactstrap

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -85,7 +85,7 @@ const NavigationNonAuth = () => {
           <Nav navbar>
             <NavItem>
               <Link to={routes.SIGN_IN}>
-                <Button type="button">Sign In</Button>
+                <Button type="button" color = "primary" >Sign In</Button>
               </Link>
             </NavItem>
           </Nav>
